2c5c9d59217d50f61ae6c52a88b6eb0b278e379f
umesh ram <ram.umesh007@gmail.com>
2023-10-09 23:31:23 +0200

    Implemented SingleToken and MultiToken

--- frontend/src/components/MultiToken/MultiToken.jsx
+++ frontend/src/components/MultiToken/MultiToken.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
+import DigitChooser from "../DigitChooser/DigitChooser";
+import { generateToken } from "../../utils/tokenGenerator";
+
 const MultiToken = () => {
+    const [tokenCount, setTokenCount] = useState(0);
+    const [validTokens, setValidTokens] = useState([]);
+    const [digits, setDigits] = useState([]);
+    const [isGenerating, setIsGenerating] = useState(false);
+
+    useEffect(() => {
+        let intervalReference;
+        if (isGenerating) {
+            intervalReference = setInterval(() => {
+                const newToken = generateToken(digits);
+                setTokenCount((previousCount) => previousCount + 1);
+                setValidTokens((previousTokens) => [...previousTokens, newToken]);
+            }, 1000);
+
+        }
+        return () => clearInterval(intervalReference);
+    }, [isGenerating]);
+
+    const onDigitsSelected = (digits) => {
+        setDigits(digits);
+    };
+    const startGeneration = () => {
+        setIsGenerating(true);
+    };
+    const stopGeneration = () => {
+        setIsGenerating(false);
+    };
     return <div>
-        MultiToken
+        <DigitChooser onDigitsSelected={onDigitsSelected} />
+        <button onClick={startGeneration}>Start Generating...</button>
+        <button onClick={stopGeneration}>Stop Generating...</button>
+        <div>{tokenCount} / {validTokens.length}</div>
+        <ul>
+            {validTokens.map((token, index) => <li key={token + index}>{token}</li>)}
+        </ul>
     </div>;
 };
 
---
+++ frontend/src/utils/tokenGenerator.js
@@ -0,0 +1,19 @@
+import { TOKEN_LENGTH } from "./contants";
+
+export const generateToken = (digits) => {
+    const min = 1;
+    const max = digits.length;
+    let count = 1;
+    let token = "";
+    for (let i = 0; i < TOKEN_LENGTH; i++) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        token += digits[randomNumber - 1].toString();
+        if (count === 4 && i + 1 !== TOKEN_LENGTH) {
+            token += "-";
+            count = 1;
+        } else {
+            count += 1;
+        }
+    }
+    return token;
+};
