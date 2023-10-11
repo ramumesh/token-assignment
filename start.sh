26885a188112626b4174353f53fbdd95893bd39e
umesh ram <ram.umesh007@gmail.com>
2023-10-11 23:23:01 +0200

    changes

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-cd frontend && yarn && yarn build
-cd .. && export PORT=8080 && node ./validator/bin/www
+echo "************************ Application Building Started *************************"
+echo ""
+echo "************************ UI Building Started **********************************"
+cd frontend && yarn install --no-lockfile
+echo "...Building frontend"
+yarn build
+echo "************************ UI Building Completed ********************************"
+echo ""
+echo "*********************** Backend Building Started ******************************"
+cd ../validator && yarn install --no-lockfile
+echo "*********************** Backend Building Completed *****************************"
+echo ""
+echo "*********************** Application Building Completed **************************"
+echo ""
+echo ""
+echo "*********************** Staring Application *************************************"
+export PORT=8080 && node ./bin/www
